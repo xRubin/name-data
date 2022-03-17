7783862397856a8d8d1138550d933b2f1e1757f0
xRubin <dalamar84@mail.ru>
2022-03-18 02:12:12 +0300

    Taldan & Tian

---
+++ src/Pathfinder/Tian/Dan/ru_RU/FemaleName.php
@@ -0,0 +1,10 @@
+<?php
+
+// Tian-Dan names appear to be modeled after Vietnamese, having both a first and middle name.
+return [
+    'Батьхьен',
+    'Докъюен',
+    'Нгокъен',
+    'Кесуан',
+    'Хайминь',
+];
